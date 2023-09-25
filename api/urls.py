4e3e5bb83c10a669a4382dba7f3b2cf936cb3232
AAhadNur <ahadnur0001@gmail.com>
2023-09-25 13:31:11 +0600

    TaskList, TaskCreate endpoints created

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
+from api import views
+
 
 urlpatterns = [
-
+    path('tasks/', views.TaskListView.as_view(),
+         name='api-task-list'),
+    path('tasks/create/', views.TaskCreateView.as_view(),
+         name='api-task-list-create'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+
+from tasks.models import Task
+from api.serializers import TaskSerializer
 
 # Create your views here.
+
+
+class TaskListView(generics.ListAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class TaskCreateView(generics.CreateAPIView):
+    queryset = Task.objects.all()[:3]
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
