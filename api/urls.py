6f2a5f2757c7224f9315a716bd553a56f430c60e
AAhadNur <ahadnur0001@gmail.com>
2023-09-25 13:36:10 +0600

    single task Retrieve, Update and delete endpoint created with proper authorization

--- api/urls.py
+++ api/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
          name='api-task-list'),
     path('tasks/create/', views.TaskCreateView.as_view(),
          name='api-task-list-create'),
+    path('tasks/<int:pk>/', views.TaskRetrieveUpdateDeleteView.as_view(),
+         name='api-task-retrieve-update-delete'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,11 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 from tasks.models import Task
 from api.serializers import TaskSerializer
+from api.permissions import IsTaskOwner
 
 # Create your views here.
 
@@ -18,3 +22,25 @@ class TaskCreateView(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
+
+
+class TaskRetrieveUpdateDeleteView(APIView):
+    permission_classes = [IsTaskOwner]
+
+    def get(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        task = get_object_or_404(Task, pk=pk)
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
